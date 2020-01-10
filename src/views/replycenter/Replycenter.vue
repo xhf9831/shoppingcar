e1d31b5b10ebc0c45494c302f22a2ffdbc305efb
xhf9831 <18215615808@163.com>
2020-01-10 22:25:22 +0800

    22:25

--- src/views/replycenter/Replycenter.vue
+++ src/views/replycenter/Replycenter.vue
@@ -15,7 +15,19 @@
     <van-tabs v-model="active"> 
       <van-tab title="待评价">
         <div class="van-content">
-          123
+          <div class="e-box" v-for="item in list" :key="item.id">
+            <div class="left">
+              <img class="e-p" :src="item.image_path" alt="">
+            </div>
+            <div class="right">
+              <div class="e-title">
+                {{item.name}}
+              </div>
+              <div class="e-reply van__marg">
+                <span class="toreply"><van-icon color="#FF4D4D" name="chat" />评论晒单</span>
+              </div>
+            </div>
+          </div>
         </div>
       </van-tab>
       <van-tab title="已评价">
@@ -32,7 +44,8 @@
  export default {
    data () {
      return {
-       active:0
+       active:0,
+       list:[]
      }
    },
    components: {
@@ -44,7 +57,8 @@
      },
      getData(){
        this.$api.tobeEvaluated().then(res=>{
-         console.log(res);
+         this.list = res.data.list
+         console.log(this.list);
        }).catch(err=>{
          console.log(err);
        })
@@ -80,5 +94,35 @@
 }
 .van-content{
   padding-top: 30px;
+  background: white;
+}
+.e-box{
+  display: flex;
+  justify-content: space-between;
+  padding: 10px 20px;
+  .left{
+    width: 150px;
+    .e-p{
+      width: 100%;
+      height: 100%;
+    }
+  }
+  .right{
+    font-size: 15px;
+    width: 100%;
+    color: #323233;
+    .e-reply{
+      position: relative;
+      top:60px;
+      text-align: right;
+      .toreply{
+        border: 1px solid #FF4D4D;
+        color: #FF4D4D;
+        font-size: 13px;
+        border-radius: 15px;
+        padding: 5px 10px;
+      }
+    }
+  }
 }
 </style>
